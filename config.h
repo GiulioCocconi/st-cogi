8e45156ffb3b183e581bf83f52f9291986bb0fe5
Giulio Cocconi <coccogiulio8@gmail.com>
2022-04-11 19:46:04 +0200

    Added variable to skip SHELL env check

--- config.h
+++ config.h
@@ -6,16 +6,19 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
  * 2: scroll and/or utmp
- * 3: SHELL environment variable
+ * 3: SHELL environment variable (if shell-env-check is 1)
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
+
+int shell_env_check = 0;
+
 static char *shell = "/bin/bash";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
